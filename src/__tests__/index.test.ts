252880d707adc4d372f17c3aac41467866d970d8
Neil Kistner <neil.kistner@gmail.com>
2019-07-28 19:01:34 -0500

    Upgrade dependencies and meta file updates
    Close #14
    Close #15
    Close #16
    Close #18

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -3,7 +3,6 @@ import * as module from '..'
 describe('gatsby-source-graphql', () => {
   it('exports functions', () => {
     expect(module).toEqual({
-      __esModule: true,
       onCreateNode: expect.any(Function),
     })
   })
